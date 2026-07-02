47943e766c62cc0e54207967d1c3e430965d651e
imen_hammami <imen.hammami@esprit.tn>
2026-07-02 07:33:24 +0100

    feat: implement user-list component with coficab styling

--- src/app/features/user-management/components/user-list/user-list.component.ts
+++ src/app/features/user-management/components/user-list/user-list.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
 })
 export class UserListComponent {
+  private userService = inject(UserService);
+  users = this.userService.getUsers();
 
+  onDelete(id: number) {
+    this.userService.deleteUser(id);
+  }
 }
